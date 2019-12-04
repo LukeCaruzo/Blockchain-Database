a07184fa9ebe599ef46f4fe07348ed7b3d38f325
LukeCaruzo <schmidt.lucas@googlemail.com>
2019-12-04 17:28:20 +0100

    hash

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,18 +9,34 @@ object Main {
   def main(args: Array[String]): Unit = {
     //val dao = new MongoDbDao("myUserAdmin", "abc123", "admin")
 
-    println(MongoDb.count)
-
-    println(MongoDb.insert(Block("test")))
+    MongoDb.insert(Block("test"))
 
     Thread.sleep(1000)
 
-    println(MongoDb.read(2))
+    val document = MongoDb.read(1)
+    document match {
+      case Some(value) => prettyPrintBlock(value)
+      case None => println("No document found")
+    }
 
     Thread.sleep(1000)
 
-    println(MongoDb.show)
+    println("Documents: " + MongoDb.count)
+
+    val documents = MongoDb.show
+    for(document <- documents) {
+      prettyPrintBlock(document)
+    }
 
     Thread.sleep(1000)
+  }
+
+  def prettyPrintBlock(block: Block): Unit = {
+    println("_id: " + block._id)
+    println("timestamp: " + block.timestamp)
+    println("previousHash: " + block.previousHash)
+    println("hash: " + block.hash)
+    println("data: " + block.data)
+    println("")
   }
 }
