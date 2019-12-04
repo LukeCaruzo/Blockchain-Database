f4bede97abec74595e5d1f81b416355802c17f88
LukeCaruzo <schmidt.lucas@googlemail.com>
2019-12-04 20:39:55 +0100

    test

--- src/test/scala/model/BlockSpec.scala
+++ src/test/scala/model/BlockSpec.scala
@@ -8,7 +8,10 @@ import org.scalatest.junit.JUnitRunner
 class BlockSpec extends WordSpec with Matchers {
   "A Block" when {
     "new" should {
-
+      val block = new Block(0, "1575488199270", "test", "test", "test")
+      "have a toString method" in {
+        block.toString should be("Block(0,1575488199270,test,test)")
+      }
     }
   }
 }
