057955068ac8b57e2e0062940eca263eec2e898f
LukeCaruzo <schmidt.lucas@googlemail.com>
2019-12-06 16:52:14 +0100

    main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,14 +9,6 @@ object Main {
   def main(args: Array[String]): Unit = {
     val dao = new MongoDb("myUserAdmin", "abc123", "admin")
 
-    /*//println(BigInt(212, new Random))
-    val privateKey = Key.sec(BigInt("291136341796021072510895648022535616155435573451899175553147951"), ECDSA.p192)
-
-    val transactionList = new TransactionList
-    transactionList.add(privateKey, Transaction("test"))
-    transactionList.add(privateKey, Transaction("test"))
-    println(transactionList)*/
-
     dao.insert(Block("test"))
 
     Thread.sleep(1000)
@@ -39,7 +31,7 @@ object Main {
     Thread.sleep(1000)
   }
 
-  def prettyPrintBlock(block: Block): Unit = {
+  private def prettyPrintBlock(block: Block): Unit = {
     println("_id: " + block._id)
     println("timestamp: " + block.timestamp)
     println("previousHash: " + block.previousHash)
