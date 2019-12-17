2da86767ec5792590df4f1dc80fe101d2f0c7c21
LukeCaruzo <schmidt.lucas@googlemail.com>
2019-12-17 22:08:13 +0100

    simplified stop mongodb

--- scripts/stop_mongodb.sh
+++ scripts/stop_mongodb.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-mongo --port 27017 --authenticationDatabase "admin" -u "admin" -p "test" <<EOF
+array=( 27017 27018 27019 )
+for i in "${array[@]}"; do
+mongo --port "$i" --authenticationDatabase "admin" -u "admin" -p "test" <<EOF
 db.adminCommand( { shutdown: 1 } )
 EOF
-
-mongo --port 27018 --authenticationDatabase "admin" -u "admin" -p "test" <<EOF
-db.adminCommand( { shutdown: 1 } )
-EOF
-
-mongo --port 27019 --authenticationDatabase "admin" -u "admin" -p "test" <<EOF
-db.adminCommand( { shutdown: 1 } )
-EOF
+done
