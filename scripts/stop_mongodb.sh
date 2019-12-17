81ccd61068e3cd36bf3e51931f63cd737d8758df
LukeCaruzo <schmidt.lucas@googlemail.com>
2019-12-17 22:32:43 +0100

    simplified stop mongodb

--- scripts/stop_mongodb.sh
+++ scripts/stop_mongodb.sh
@@ -3,6 +3,7 @@
 array=( 27017 27018 27019 )
 for i in "${array[@]}"; do
 mongo --port "$i" --authenticationDatabase "admin" -u "admin" -p "test" <<EOF
-db.adminCommand( { shutdown: 1 } )
+use admin
+db.shutdownServer();
 EOF
 done
