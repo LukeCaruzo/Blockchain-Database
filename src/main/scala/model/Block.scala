55fd1e6a6f748c04d35ad222974ff84229256956
LukeCaruzo <schmidt.lucas@googlemail.com>
2019-12-17 22:40:51 +0100

    simplified block

--- src/main/scala/model/Block.scala
+++ src/main/scala/model/Block.scala
@@ -4,14 +4,6 @@ object Block {
   def apply(data: String): Block = new Block(-1, "", "", "", data)
 }
 
-/** Block of the blockchain.
- *
- * @param _id          Identifier of the block
- * @param timestamp    Timestamp of the block
- * @param previousHash Hash of the previous block
- * @param hash         Hash of the block
- * @param data         Data stored in the block
- */
 case class Block(var _id: Long, var timestamp: String, var previousHash: String, var hash: String, data: String) {
   override def toString: String = this.getClass.getSimpleName + "(" + _id + "," + timestamp + "," + previousHash + "," + data + ")"
 }
