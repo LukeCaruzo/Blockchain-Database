8bef9950383d0797ae184f73776c2cd89c7678a6
LukeCaruzo <schmidt.lucas@googlemail.com>
2019-12-17 15:37:19 +0100

    tests

--- src/test/scala/model/BlockSpec.scala
+++ src/test/scala/model/BlockSpec.scala
@@ -6,9 +6,16 @@ import org.scalatest.matchers.should.Matchers
 class BlockSpec extends WordSpec with Matchers {
   "A Block" when {
     "new" should {
-      val block = new Block(0, "1575488199270", "test", "test", "test")
+      val block = Block("test")
+      "be initalized with" in {
+        block._id should be(-1)
+        block.timestamp should be("")
+        block.previousHash should be("")
+        block.hash should be("")
+        block.data should be("test")
+      }
       "have a toString method" in {
-        block.toString should be("Block(0,1575488199270,test,test)")
+        block.toString should be("Block(-1,,,test)")
       }
     }
   }
