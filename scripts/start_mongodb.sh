4ba252518ac0d14eb60dd069a637714ff0d53307
LukeCaruzo <schmidt.lucas@googlemail.com>
2020-01-09 17:28:18 +0100

    loop

--- scripts/start_mongodb.sh
+++ scripts/start_mongodb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# TODO: In Schleife realisieren
-mongod --logpath /usr/local/var/log/mongodb/mongo.log --logappend --fork --replSet rs --port 27017 --dbpath ../data/replicaset0 --bind_ip localhost,server0 --keyFile ../data/keyfile
-mongod --logpath /usr/local/var/log/mongodb/mongo.log --logappend --fork --replSet rs --port 27018 --dbpath ../data/replicaset1 --bind_ip localhost,server1 --keyFile ../data/keyfile
-mongod --logpath /usr/local/var/log/mongodb/mongo.log --logappend --fork --replSet rs --port 27019 --dbpath ../data/replicaset2 --bind_ip localhost,server2 --keyFile ../data/keyfile
+declare -ga mservers=([0]="27017" [1]="27018" [2]="27019")
+for i in "${!mservers[@]}"; do
+  mongod --logpath /usr/local/var/log/mongodb/mongo.log --logappend --fork --replSet rs --port "${mservers[${i}]}" --dbpath ../data/replicaset${i} --bind_ip localhost,server${i} --keyFile ../data/keyfile
+done
 
 # Shows running MongoDB instances
 ps aux | grep -v grep | grep mongod
